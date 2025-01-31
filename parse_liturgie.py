7237ce2845a497cd27617a63d62db21315ba6c41
agrauss <antoniegrauss@hotmail.com>
2025-01-31 17:25:50 +0100

    Regex parser

--- parse_liturgie.py
+++ parse_liturgie.py
@@ -1,4 +1,5 @@
 from typing import List
+import re
 
 class Dienst:
     def __init__(self, raw_data) -> None:
@@ -9,6 +10,51 @@ class Dienst:
         print("Dienst data: " + self.dienst_data)
         print("")
         print("Liturgie: " + self.liturgie)
+        
+def fix_liturgie_line_breaks(liturgie):
+    lines = []
+    
+    # Interesting case about liturgie with no line breaks, but only `;`
+    max_semicolons = 3
+    if liturgie.count(";") > max_semicolons:
+        print("Many semicolons")
+        print(liturgie)
+    
+    for line in liturgie.split("\n"):
+        # Always add first to to list
+        if not lines:
+            if not line:
+                continue
+            lines.append(line)
+            continue
+            
+        # If line ends on a weird character, 
+        # or the next line starts on a weird character
+        # add it to the previous line
+        last_char_last_line = lines[-1].strip()[-1]
+        end_separators = [".", ":", ",", "‘"]
+        first_char_this_line = line.strip()[0]
+        start_separators = [".", ":", ";", ",", "-", "(", "–"]
+        if last_char_last_line in end_separators or \
+            first_char_this_line in start_separators:
+                
+            lines[-1] += " " + line
+            continue
+        
+        # Special case when line is only lied, example:
+        #   lied 
+        #   onze schuilplaats is god
+        #   lied 
+        #   in ons hart geboren  (sela)
+        #   gebed
+        if lines[-1].strip().endswith("lied") or lines[-1].strip().endswith("zingen"):
+            lines[-1] += " " + line
+            continue
+        
+        if line != "":
+            lines.append(line)
+    
+    return "\n".join(lines)
 
 def read_in_diensten(file_name):
     data = open(file_name, "r")
@@ -16,7 +62,7 @@ def read_in_diensten(file_name):
     print(headers)
     
     # Liturgien zijn multilines met quotes "" eromheen
-    diensten_en_data = data.read().split("\"")
+    diensten_en_data = data.read().lower().split("\"")
     
     # Maak een Dienst object voor elke dienst
     diensten = []
@@ -28,52 +74,44 @@ def read_in_diensten(file_name):
         
     return diensten
 
-# TODO: check voor liederen van meerdere regels
 def parse_liederen(liturgie: str) -> List[str]:
-    # Parse alle regels 1 voor 1, houd alleen degene met een lied erin
-    liederen = [lied for lied in [parse_lied(x) for x in liturgie.split("\n")] if lied != None]
-    return liederen
+    regex = r'.*(?:lied|zingen|opw |opw. |opwekking |ps |ps. |psalm |psalmen |psalmproject |nlb |lb: |gezang |tijdens |couplet).*\n'
+    # regex = r".*(?:lied).*"
+    preprocessed_liturgie = fix_liturgie_line_breaks(liturgie.lower())
+    return re.findall(regex, preprocessed_liturgie)
 
-# Patterns:
-# Indent/Niets + Lied/Slotlied/Zegenlied/Zingen + Niets/:/.
+def test_parse_full_liturgie():
+    assert(len(parse_liederen("Liturgie: welkom, votum & groet\n\
+opwekking 464  \n\
+wees stil voor het aangezicht van god\n\
+leefregel\n\
+opwekking 876 \n\
+voor altijd heilig\n\
+kindermoment over bidden\n\
+gebed\n\
+inleiding op de doop\n\
+opwekking 599 \n\
+kom tot de vader\n\
+doopvragen en heilige doop\n\
+lied:  zegen voor de kinderen  - sela\n\
+bijbellezing: lucas 11: 1-13\n\
+preek\n\
+opwekking 436 \n\
+onze vader\n\
+mededelingen\n\
+voorbedegebed\n\
+collecte: financiële ondersteuning (diaconie)\n\
+slotlied:  de kerk van alle tijden  (lev)\n\
+zegen")
+       ) == 6   )
 
-# Lied types:
-# Note: Kan ook de titel van een opwekking of lied zijn zonder iets erbij
-# Opw PvN Sela Psalm Ps LB NLB DNP Svg Schrijvers voor gerechtigheid
-def parse_lied(lied: str) -> str:
-    if "lied" in lied.lower() or "zingen" in lied.lower():
-        if ":" in lied:
-            return lied.split(":")[1].strip()
-        elif "." in lied:
-            return lied.split(".")[1].strip()
-        else:
-            return lied.strip()
-    return None
-
-def test_parse_lied():
-    assert(parse_liederen("Opw 585") == ["Opw 585"])
-    assert(parse_liederen("Lied: Opw 585") == ["Opw 585"])
-    assert(parse_liederen("Lied. Opw 585") == ["Opw 585"])
-    
-    # Moet leeg zijn
-    assert(parse_liederen("De kerkdienst wordt gehouden in de Immanuelkerk en wordt ook uitgezonden via Youtube\nKlik \nhier\n om de dienst van deze week te bekijken") 
-           == [])
-    
-    # Liederen van meerdere regels :(
-    assert(parse_liederen("Slotlied: \nDe kerk van alle tijden \n(LEV)") 
-           == ["De kerk van alle tijden (LEV)"])
-    assert(parse_liederen("Lied: \nZegen voor de kinderen\n - Sela") 
-           == ["Zegen voor de kinderen - Sela"])
-    
-    # Lied met nog een nummer erbij
-    assert(parse_liederen("Lied 1: \nJoy to the world\nLied 2: \nKomt allen tezamen") 
-           == ["Joy to the world", "Komt allen tezamen"])
-
-def main():
+def main(): 
     diensten = read_in_diensten(file_name="zangdiensten_2023_2024.csv")
     for dienst in diensten:
         dienst.print()
-        print("\n")
+        print("Liederen:")
+        print(parse_liederen(dienst.liturgie))
+        print()
 
 if __name__ == '__main__':
     main()
