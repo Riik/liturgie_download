badbec044a4312c5eb13b31bb343573e1ab23546
agrauss <antoniegrauss@hotmail.com>
2025-02-01 14:01:17 +0100

    Sla op als csv en documentatie

--- parse_liturgie.py
+++ parse_liturgie.py
@@ -1,15 +1,46 @@
 from typing import List
 import re
 
+# Script om diensten in te lezen, liederen eruit te parsen en weer in csv
+# format op te slaan
+# Notes:
+# Dit script leest `zangdiensten_2023_2024.csv` in met alle diensten.
+# Deze .csv moet je zelf maken en een paar aanpassingen doen aan de data:
+#   1. Multi line liturgie wordt ge-exporteerd met "" om de tekst, dus als er nog
+#   "" in de tekst staan, dan moet je die eruit halen
+#   2. Zorg er ook voor dat in de Datum, Stijl, Bezetting geen , staan
+#   want dat is de delimiter voor de csv
+# De output is een liederen.csv die de liturgie vervangt voor de liederen
+
+
 class Dienst:
     def __init__(self, raw_data) -> None:
         self.liturgie = raw_data[1]
-        self.dienst_data = raw_data[0]
+        self.dienst_data = raw_data[0].replace("\n", "")
+        self.find_liederen()
         
     def print(self):
         print("Dienst data: " + self.dienst_data)
+        print("Liederen:")
+        for id, lied in enumerate(self.liederen):
+            print(str(id+1) + ":" + lied)
         print("")
         print("Liturgie: " + self.liturgie)
+        
+    def find_liederen(self):
+        liederen = parse_liederen(self.liturgie)
+        self.liederen = [lied.strip().replace("\n", "").replace(",", "") for lied in liederen]
+        
+    def write_csv(self, file_name):
+        delimiter  = ","
+        try:
+            with open(file_name, "a") as file:
+                file.write(self.dienst_data)
+                file.write(delimiter.join(self.liederen) + "\n")
+                print(delimiter.join(self.liederen) + "\n")
+        except Exception as e:
+            print("Error writing to file")
+            print(e)
         
 def fix_liturgie_line_breaks(liturgie):
     lines = []
@@ -17,8 +48,7 @@ def fix_liturgie_line_breaks(liturgie):
     # Interesting case about liturgie with no line breaks, but only `;`
     max_semicolons = 3
     if liturgie.count(";") > max_semicolons:
-        print("Many semicolons")
-        print(liturgie)
+        print("Dienst with many semicolons found")
     
     for line in liturgie.split("\n"):
         # Always add first to to list
@@ -59,7 +89,6 @@ def fix_liturgie_line_breaks(liturgie):
 def read_in_diensten(file_name):
     data = open(file_name, "r")
     headers = data.readline().strip("\n").split(",")
-    print(headers)
     
     # Liturgien zijn multilines met quotes "" eromheen
     diensten_en_data = data.read().lower().split("\"")
@@ -72,7 +101,7 @@ def read_in_diensten(file_name):
             break
         diensten.append(Dienst(diensten_en_data[i:i+2]))
         
-    return diensten
+    return diensten, headers
 
 def parse_liederen(liturgie: str) -> List[str]:
     regex = r'.*(?:lied|zingen|opw |opw. |opwekking |ps |ps. |psalm |psalmen |psalmproject |nlb |lb: |gezang |tijdens |couplet).*\n'
@@ -105,13 +134,23 @@ slotlied:  de kerk van alle tijden  (lev)\n\
 zegen")
        ) == 6   )
 
+def write_headers(file_name, headers):
+    try:
+        with open(file_name, "w") as file:
+            file.write(",".join(headers) + "\n")
+    except Exception as e:
+        print("Error writing to file")
+        print(e)
+
 def main(): 
-    diensten = read_in_diensten(file_name="zangdiensten_2023_2024.csv")
+    diensten, headers = read_in_diensten(file_name="zangdiensten_2023_2024.csv")
+    
+    liederen_csv = "liederen.csv"
+    headers[-1] = ("Liederen")
+    write_headers(liederen_csv, headers)
     for dienst in diensten:
+        dienst.write_csv(liederen_csv)
         dienst.print()
-        print("Liederen:")
-        print(parse_liederen(dienst.liturgie))
-        print()
 
 if __name__ == '__main__':
     main()
